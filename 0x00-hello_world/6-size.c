e9a2f22d2b22160fdac2b733715dfb1bd116baca
root <deboraholofin43@gmail.com>
2022-06-30 20:42:46 -0700

    TASK 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
 
 /**
- *main - Entry point
+ *main - prints the 
  *Return: Always 0 (success)
  *size of evaluates the size of variables
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %d byte(s)\n", sizeof(long int));
-printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
+int i;
+char c;
+float f;
+long int l;
+long long int ll;
+  
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
+printf("Size of an int: %lu byte(s)\n", sizeof(t));
+printf("size of a long int: %lu byte(s)\n", sizeof(l));
+printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("Size of a float: %lu byte(s)\n", sizeof(f));
 return (0);
 }
