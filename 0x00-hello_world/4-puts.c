64d6d76015587f466820115db124c9ef1c8e1e15
root <deboraholofin43@gmail.com>
2022-06-30 17:28:07 -0700

    TASK 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-puts( "%s \n"Programming is like building a multilingual puzzle );
+#include <stdio.h>
+
+/**
+    *main -- Entry point. It also where variables, instructions  and the likes are issued out
+    *return -- if return is 0 (success executing the code) if o  therwise its a failure
+*/
+
+int main(void)
+{
+   
+    puts("%s \n"Programming is like building a multilingual puz    zle \n);
+    return (0);
+};
