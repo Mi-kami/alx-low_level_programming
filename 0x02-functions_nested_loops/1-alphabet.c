a52b7f36fda203942e5581a2618ad60ca4c01b2d
root <deboraholofin43@gmail.com>
2022-07-06 19:59:11 -0700

    TASK 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,17 @@
 */
 
 void print_alphabet(void)
-}
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
 {
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++){
 putchar(ch);
-}
+ }char ch;
+ for (ch = 'a' ; ch <= 'z' ; ch++)
+   {
+     putchar(ch);
+   }
+ putchar('\n');
+ return (0);~
 putchar('\n');
 return (0);
 }
