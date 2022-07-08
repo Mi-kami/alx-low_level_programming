8836f2bdd65f37fda469ddfaed2dd0aa52003796
root <deboraholofin43@gmail.com>
2022-07-07 18:33:08 -0700

    updating main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,19 @@ int _putchar(char c);
 int _isupper(int c);
 
 
+/**
+ * function that checks for a digit (0 through 9)
+*/
+int _isdigit(int c);
+
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
 #endif 
