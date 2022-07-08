7830acc3a380de921ce9f085af61146e0fb80366
root <deboraholofin43@gmail.com>
2022-07-07 19:13:31 -0700

    TASK 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,8 +7,18 @@
 
 void print_line(int n)
 {
-int times = n;
-for (times = n; times > 0; times--)
-_putchar('_');
+if (n <= 0)
+{
 _putchar('\n');
 }
+else
+{
+int i;
+for (i = 1; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
+
