3b9dc654af984f955bd75c48f7ad95fc58462fdd
root <deboraholofin43@gmail.com>
2022-07-07 18:56:56 -0700

    TASK 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_most_numbers -  prints the numbers, from 0 to 9, followed by a new line.,skip 2 and 4
+ * print_most_numbers -  prints the numbers, from 0 to 9,skip 2 and 4
  * @i - digits being checked
  * Return: Always 0.
  */
-
 
 void print_most_numbers(void)
 {
