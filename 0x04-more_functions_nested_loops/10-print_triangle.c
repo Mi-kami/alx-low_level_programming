013838bac625f3169c4657ccdcb2fefbcfb3f34a
root <deboraholofin43@gmail.com>
2022-07-08 19:53:57 -0700

    TASK 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - print traingle with #'s with given size
+ * @size: size of triangle to draw
+ */
+
+void print_triangle(int size)
+{
+int x;
+int y;
+int z;
+if (size <= 0)
+_putchar('\n');
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < (size - x); y++)
+_putchar(' ');
+for (z = 0; z < x; z++)
+_putchar('#');
+_putchar('\n');
+}
+}
