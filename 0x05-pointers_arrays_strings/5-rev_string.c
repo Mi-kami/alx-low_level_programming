3a1b8f1745d0d1c9376b5f73760e28dd59bd53ef
root <deboraholofin43@gmail.com>
2022-07-12 21:36:02 -0700

    TASK 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,12 @@ length1 = 0;
 length2 = 0;
 while (s[length1] != '\0')
 length1++;
-length2 = length1 - 1;  
-for ( i = 0; i < length1; i++)
+length2 = length1 - 1;
+for( i = 0; i < length1 / 2; i++)
 {
 tmp = s[i];
 s[i] = s[length2];
 s[length2] = tmp;
-length2 -= 1; 
+length2 -= 1;
 }
 }
