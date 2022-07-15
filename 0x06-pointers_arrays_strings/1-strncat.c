7da25f1dadaa0e0fa831b5dd5c25576fa89dbdbd
root <deboraholofin43@gmail.com>
2022-07-14 18:29:26 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main .h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n: nth
+ * Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+strncat(dest, src, n);
+return (dest);
+}
