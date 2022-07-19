d18ef0578398c1dca03b6d6a8cb5b3f64d70b0e3
root <deboraholofin43@gmail.com>
2022-07-18 19:17:31 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strspn - return length of string that matches values consistently
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int a = 0, b, t = 0;
+while (accept[a])
+{
+b = 0;
+while (s[b] != 32)
+{
+if (accept[a] == s[b])
+{
+t++;
+}
+b++;
+}
+a++;
+}
+return (t);
+}
