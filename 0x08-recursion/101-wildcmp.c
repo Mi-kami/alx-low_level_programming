5c1f990b8cef83bff2d843d24f103717e34cbf99
root <deboraholofin43@gmail.com>
2022-07-22 16:16:37 -0700

    ignored files

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,17 +16,17 @@ char *move(char *s2);
 int wildcmp(char *s1, char *s2)
 {
 /**
- * @sum - this is going to be a sum of return values
+ * this is going to be a sum of return values
 */
 int sum = 0;
-
 /**
+ * @s1: first string to compare
+ * @s2: second string to compare
  * if we reach the end of s1 and the char in s2 is a *
  * and if the next chars of s2 are *, return 1
 */
 if (*s1 == '\0' && *s2 == '*' && !*move(s2))
 return (1);
-
 /**
  * if the chars are equal in both strings,
  * if we reached the end of s1, return 1
@@ -67,7 +67,6 @@ return (!!sum);
 }
 return (0);
 }
-
 /**
  * bandersnatch - checks recursively for all the paths when the
  * characters are equal
@@ -76,7 +75,6 @@ return (0);
  *
  * Return: return value of wildcmp() or of itself
 */
-
 int bandersnatch(char *s1, char *s2)
 {
 /**
@@ -84,21 +82,18 @@ int bandersnatch(char *s1, char *s2)
  * if chars are equal, return the return value of wildcmp()
  * increment s1 by 1, not s2
 */
-
 if (*s1 == '\0')
 return (0);
 if (*s1 == *s2)
 return (wildcmp(s1, s2));
 return (bandersnatch(s1 + 1, s2));
 }
-
 /**
  * *move - moves the current char past the *
  * @s2: string to iterate over
  *
  * Return: the address of the character after the *
 */
-
 char *move(char *s2)
 {
 /**
