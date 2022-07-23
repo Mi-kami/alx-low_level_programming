14afea052e18540d6ffb4ce794080bb37f287771
root <deboraholofin43@gmail.com>
2022-07-22 20:02:16 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int size = n;
-if (size > 0)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-int i;
-for (i = 0; i < 10; i++)
 dest[i] = src[i];
 }
 return (dest);
