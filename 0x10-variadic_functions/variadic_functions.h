e874336615421f9b0c537cbb71a692d8112ff7aa
root <deboraholofin43@gmail.com>
2022-08-04 20:09:50 -0700

    header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_c(va_list c);
+void print_i(va_list i);
+void print_f(va_list f);
+void print_s(va_list s);
+typedef struct fmt
+{
+char *fmt;
+void (*f)();
+} print_t;
+
+#endif
