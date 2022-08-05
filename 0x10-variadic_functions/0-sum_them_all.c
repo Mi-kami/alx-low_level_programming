ae6c42471d9497ff52c66cc159ba09ef02fba774
root <deboraholofin43@gmail.com>
2022-08-04 20:17:20 -0700

    Task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,18 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-unsigned int i, sum;
 va_list nums;
 va_start(nums, n);
-if (n == 0)
-return (0);
+if (n != 0)
+{
+unsigned int i;
+unsigned int sum = 0;
 for (i = 0; i < n; i++)
 {
 sum += va_arg(nums, int);
 }
 va_end(nums);
 return (sum);
+}
+return (0);
 }
